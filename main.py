3df085445bb66dea18585354965c8d09cf89f27c
itssaimaKhan <samiakhan8754@gmail.com>
2025-03-02 23:13:24 +0500

    main

--- main.py
+++ main.py
@@ -1,74 +1,74 @@
-name : str = ' (Governor Initiative for AI and computing)'
-print("GIAIC stand for : ",name)
+# name : str = ' (Governor Initiative for AI and computing)'
+# print("GIAIC stand for : ",name)
 
-print("Hello World from Asharib")
+# print("Hello World from Asharib")
 
-def giaic():
-    print("Hello World from student")
+# def giaic():
+#     print("Hello World from student")
 
 
 
-string (str)
-integen (bool)
-float (float)
-boolen (bool)
-list (list)
-tuple ( tuple)
-dictionary (dict)
-set (set)
-None (NoneType)
-frozen set (frozenset)
+# string (str)
+# integen (bool)
+# float (float)
+# boolen (bool)
+# list (list)
+# tuple ( tuple)
+# dictionary (dict)
+# set (set)
+# None (NoneType)
+#frozen set (frozenset)
 
-1
-string 
-text:str = "Hello World"
-print(text)
+# 1
+# string 
+# text:str = "Hello World"
+# print(text)
 
-id_Card:str = """Name: Hamza
-Father Name: Muhammad Aslam
-Age : 2o
-"""
-print(id_Card)
+# id_Card:str = """Name: Hamza
+# Father Name: Muhammad Aslam
+# Age : 2o
+# """
+# print(id_Card)
 
-2
-number
-num :int = 35
-print(type(num))
+# 2
+# number
+# num :int = 35
+# print(type(num))
 
-3
-float
-float_num : float = 3.5
-print(type(float_num))
+# 3
+# float
+# float_num : float = 3.5
+# print(type(float_num))
 
-4
-boolen
-is_true : bool = True
-is_false : bool = False
-print(type(is_true))
+# 4
+# boolen
+# is_true : bool = True
+# is_false : bool = False
+# print(type(is_true))
 
-5
-list
- fruit:list[str] = ["Apple","Banana","Orange"]
- print(type(fruit))
- print(fruit[0])
- print(fruit[-1])
+# 5
+# list
+#  fruit:list[str] = ["Apple","Banana","Orange"]
+#  print(type(fruit))
+#  print(fruit[0])
+#  print(fruit[-1])
 
-6
-tuple
-tuple_data:tuple = ("Apple","Banana","Orange")
-print(type(tuple_data))
-print(tuple_data[0])
-print(tuple_data[-1])
+# 6
+# tuple
+# tuple_data:tuple = ("Apple","Banana","Orange")
+# print(type(tuple_data))
+# print(tuple_data[0])
+# print(tuple_data[-1])
 
-7
-dictionary
-user_data:dict [str,str] = {
-    "name":"Hamza",
-    "age":"20",
-}
-print(type(user_data))
-print(user_data)
-print(user_data["name"])
+# 7
+# dictionary
+# user_data:dict [str,str] = {
+#     "name":"Hamza",
+#     "age":"20",
+# }
+# print(type(user_data))
+# print(user_data)
+# print(user_data["name"])
 
     
 
